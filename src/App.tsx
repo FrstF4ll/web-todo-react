d802ba78d155723b315afad933d910484e358050
FrstF4ll <hydralixe@gmail.com>
2026-02-10 13:45:38 +0100

    feat: make input section box fit its content.
    - Change 'main > *' for 'section'.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TodoInputs } from './Todos';
 
 const Title = () => {
   return (
-    <header id="heading" className='flex-column'>
+    <header id="heading" className="flex-column">
       <h1>Welcome</h1>
       <p className="status-message">Status message</p>
     </header>
