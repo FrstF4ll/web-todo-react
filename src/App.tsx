9e5da97f2b8c9cce68191af08e0df2ebb1bba807
FrstF4ll <hydralixe@gmail.com>
2026-02-10 13:18:43 +0100

    feat: create 'add todo' button.
    - Create a title component
    - Separate some ids and class name

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css';
 import { TodoInputs } from './Todos';
 
+const Title = () => {
+  return (
+    <header className="heading">
+      <h1>Welcome</h1>
+      <p className="status-message">Status message</p>
+    </header>
+  );
+};
+
 const App = () => {
   return (
     <main>
-      <section className='flex-column'>
-        <header className='heading'>
-          <h1>Welcome</h1>
-          <p className='status-message'>Status message</p>
-        </header>
-        <TodoInputs/>
+      <section className="flex-column">
+        <Title />
+        <TodoInputs />
       </section>
     </main>
   );
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,8 +1,12 @@
 export const TodoInputs = () => {
-    return ( <form className='create-todo'>
-        <div>
-          <input type="text" /><input type="date" />
-        </div>
-        <textarea/>
-        </form>)
-}
+  return (
+    <form id="todo-inputs" className="flex-column">
+      <div id="date-title-wrapper">
+        <input type="text" />
+        <input type="date" />
+      </div>
+      <textarea />
+      <button type="button">Add to list</button>
+    </form>
+  );
+};
