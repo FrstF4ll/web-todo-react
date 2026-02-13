4b7cda191b95d26c896eb882b3ecd4ddec464393
FrstF4ll <hydralixe@gmail.com>
2026-02-13 08:19:29 +0100

    feat: implement placeholder for description and title inputs.

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -2,10 +2,10 @@ export const TodoInputs = () => {
   return (
     <form id="todo-inputs" className="flex-column">
       <div id="date-title-wrapper">
-        <input type="text" />
+        <input type="text" placeholder="Enter a title..."/>
         <input type="date" />
       </div>
-      <textarea />
+      <textarea placeholder="Enter a description..."/>
       <button type="button">Add to list</button>
     </form>
   );
