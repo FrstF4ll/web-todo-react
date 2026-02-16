5e95762e0f3f483b4202a63bc78c4cf94681d706
FrstF4ll <hydralixe@gmail.com>
2026-02-16 13:48:40 +0100

    feat: implement function to retrieve todos from api

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,12 +1,12 @@
-  const queryHeader = { 
-    'accept': 'application/json', 
-    'Range-Unit': 'items' 
+const queryHeader = {
+  accept: 'application/json',
+  'Range-Unit': 'items',
 };
 
-export async function getData(){
+export async function getData() {
   const response = await fetch('https://api.todos.in.jt-lab.ch:443/todos', {
     method: 'GET',
-    headers: queryHeader
+    headers: queryHeader,
   });
-  return response.json()
+  return response.json();
 }
