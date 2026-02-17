9f2c2f2df83335ad2139583ec335d978457f6481
FrstF4ll <hydralixe@gmail.com>
2026-02-17 08:12:50 +0100

    feat: create function to post datas

---
+++ src/API/PostData.tsx
@@ -0,0 +1,10 @@
+import type { Todos } from "../Interfaces";
+
+export async function postData(todo: Todos) {
+  const response = await fetch('https://api.todos.in.jt-lab.ch:443/todos', {
+    method: 'POST',
+    headers: { 'accept': 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify(todo)
+  });
+  return response.json();
+}
