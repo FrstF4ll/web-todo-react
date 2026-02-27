431e1d31ae5be5f84f632878134fbbab63f178e9
FrstF4ll <hydralixe@gmail.com>
2026-02-27 08:25:47 +0100

    refactor: create custom type for todo form  component' props

--- src/ui/menu/inputs/TodoForm.tsx
+++ src/ui/menu/inputs/TodoForm.tsx
@@ -1,9 +1,14 @@
 import mainMenuStyles from '../MainMenu.module.css';
 import type { ReactNode } from 'react';
 
-export const TodoForm = ({ children }: { children: ReactNode }) => {
+interface TodoFormProps {
+  children: ReactNode;
+  onSave: () => void; // Le nom de variable que tu as demandé
+}
+
+export const TodoForm = ({ children, onSave }: TodoFormProps) => {
   return (
-    <form className={`flex-column ${mainMenuStyles.todoInputs}`}>
+    <form className={`flex-column ${mainMenuStyles.todoInputs}`} onSubmit={onSave}>
       {children}
     </form>
   );
