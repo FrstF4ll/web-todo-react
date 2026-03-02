f5d62f9e294a105a72dec0a865a99b410b2874d3
FrstF4ll <hydralixe@gmail.com>
2026-03-02 09:39:58 +0100

    refactor: delete 'TodosWrapper'

--- src/ui/todos/container/TodosWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import styles from '../TodoList.module.css';
-
-export const TodosWrapper = ({ children }: { children: ReactNode }) => {
-  return <ul className={`flex-column ${styles.todoList}`}>{children}</ul>;
-};
